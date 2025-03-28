261b1142afc2b31f51df348539577715eab5e1b2
Bilen Liubov <bilenliubov48@mail.ru>
2025-03-28 15:05:08 +0300

    add new test

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,34 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
+    const input = [
         "Гарри Поттер",
         "Властелин Колец",
         "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+      ];
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+
+    const output = sorting.sortByName(input);
+    expect(output).toEqual(expected);
+  });
+});
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    const input = [
+      "Гарри Потер",
+      "Гарри Потер",
+    ];
+    const expected = [
+      "Гарри Потер",
+      "Гарри Потер",
+    ];
+
+const output = sorting.sortByName(input);
+    expect(output).toEqual(expected);
   });
 });
