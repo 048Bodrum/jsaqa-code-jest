0ee783ab7dccaf9a2c712ed624ef693339fd34a0
Bilen Liubov <bilenliubov48@mail.ru>
2025-03-28 15:13:56 +0300

    change sortByName.test.js

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,35 +1,35 @@
 const sorting = require("../../app");
 
 describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
-    const input = [
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ];
-    const expected = [
-      "Властелин Колец",
-      "Волшебник изумрудного города",
-      "Гарри Поттер",
-    ];
+    it("Books names should be sorted in ascending order", () => {
+        const input = [
+            "Гарри Поттер",
+            "Властелин Колец",
+            "Волшебник изумрудного города",
+        ];
+        const expected = [
+            "Властелин Колец",
+            "Волшебник изумрудного города",
+            "Гарри Поттер",
+        ];
 
-    const output = sorting.sortByName(input);
-    expect(output).toEqual(expected);
-  });
+        const output = sorting.sortByName(input);
+        expect(output).toEqual(expected);
+    });
 });
 
 describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
-    const input = [
-      "Гарри Потер",
-      "Гарри Потер",
-    ];
-    const expected = [
-      "Гарри Потер",
-      "Гарри Потер",
-    ];
+    it("Books names should be sorted in ascending order", () => {
+        const input = [
+            "Гарри Потер",
+            "Гарри Потер ",
+        ];
+        const expected = [
+            "Гарри Потер",
+            "Гарри Потер",
+        ];
 
-const output = sorting.sortByName(input);
-    expect(output).toEqual(expected);
-  });
+        const output = sorting.sortByName(input);
+        expect(output).toEqual(expected);
+    });
 });
